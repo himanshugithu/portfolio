544fce3f349754cc043be9f79929a3e1a0e4fcdf
himanshugithu <fanibharehimanshu@gmail.com>
2023-12-28 23:50:00 +0530

    project update

--- portfolio.py
+++ portfolio.py
@@ -1,5 +1,7 @@
 import streamlit as st
-# st.set_page_config(layout='wide')
+import pandas as pd
+
+st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1 :
@@ -15,3 +17,13 @@ with col2:
     st.info(content)
 content2 = """My Projects"""
 st.write(content2)
+col3,col4 = st.columns(2)
+df = pd.read_csv("data.csv",sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
