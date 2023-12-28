a240c986758cb29a2a7b2639bdd2bb444a596c81
himanshugithu <fanibharehimanshu@gmail.com>
2023-12-29 01:25:38 +0530

    send email

--- pages/contact.py
+++ pages/contact.py
@@ -1,3 +1,9 @@
 import streamlit as st
-
-st.header("contact us")
+# wcvh dkrs okfy swom
+st.header("contact Me")
+with st.form(key="email_form"):
+    user_email = st.text_input("Email address")
+    messege = st.text_area("Yout message")
+    button = st.form_submit_button("Submit")
+    if button:
+        print("pressed")
