dae6479bcaa14fab0cb15b4f411c73fc15079868
himanshugithu <fanibharehimanshu@gmail.com>
2023-12-28 18:06:26 +0530

    first commit

---
+++ portfolio.py
@@ -0,0 +1,15 @@
+import streamlit as st
+# st.set_page_config(layout='wide')
+col1, col2 = st.columns(2)
+
+with col1 :
+    st.image('image/himanshu.jpg',width=200)
+
+with col2:
+    st.title("Himanshu Fanibhare")    
+    content="""Hello,
+               My name is himanshu 
+               and current pursuing Btech
+               from the st vincent pallotti collage"""
+    # st.write(content)
+    st.info(content)
