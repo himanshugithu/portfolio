269f04230a9a24c02185243729d8759150a454c4
himanshugithu <fanibharehimanshu@gmail.com>
2023-12-29 00:46:31 +0530

    contact us

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(layout='wide')
+# st.set_page_config(layout='wide') 
 col1, col2 = st.columns(2)
 
 with col1 :
@@ -17,13 +17,17 @@ with col2:
     st.info(content)
 content2 = """My Projects"""
 st.write(content2)
-col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([1.5,0.5,1.5])
 df = pd.read_csv("data.csv",sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("image/"+ row["image"])
+        st.write(f"[source code]({row["url"]})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
